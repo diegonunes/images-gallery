d270b9adfb848696185fc8d0e236c79d6c441222
Diego Nunes <diegonunesdev@gmail.com>
2025-06-29 00:09:01 -0300

    controlled search component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search.jsx';
+import {useState} from 'react';
 
 const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
   return (
       <>
         <Header title="Image Gallery"/>
+        <Search word={word} setWord={setWord}
+                handleSubmit={handleSearchSubmit}/>
       </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import {Container, Navbar} from 'react-bootstrap';
 
+const navbarStyle = {
+  backgroundColor: 'lightblue',
+};
+
 const Header = ({title}) => {
   return (
-      <Navbar bg="light" variant="light">
+      <Navbar variant="light" style={navbarStyle}>
         <Container>
           <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Container>
