9ba75016181d05617f770d7fc0312b984c8ba14f
Diego Nunes <diegonunesdev@gmail.com>
2025-07-23 03:24:50 -0300

    toast notifications for the frontend app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search.jsx';
@@ -7,6 +8,7 @@ import {Col, Container, Row} from 'react-bootstrap';
 import Welcome from './components/Welcome.jsx';
 import axios from 'axios';
 import Spinner from './components/Spinner.jsx';
+import {ToastContainer, toast} from 'react-toastify';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
 
@@ -21,8 +23,10 @@ const App = () => {
         const res = await axios.get(`${API_URL}/images`);
         setImages(res.data || []);
         setLoading(false);
+        toast.success('Saved images downloaded');
       } catch (error) {
         console.log(error);
+        toast.error(error.message);
       }
     };
     getSavedImages();
@@ -34,8 +38,10 @@ const App = () => {
     try {
       const res = await axios.get(`${API_URL}/new-image?query=${word}`);
       setImages([{...res.data, title: word}, ...images]);
+      toast.info(`New image ${word.toUpperCase()} was found`);
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
 
     setWord('');
@@ -45,10 +51,14 @@ const App = () => {
     try {
       const res = await axios.delete(`${API_URL}/images/${id}`);
       if (res.data?.deleted_id) {
+        toast.warn(`Image ${images.find((i) => i.id === id).
+            title.
+            toUpperCase()} was deleted`);
         setImages(images.filter((image) => image.id !== id));
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
 
@@ -64,9 +74,11 @@ const App = () => {
                 image.id === id ? {...image, saved: true} : image,
             ),
         );
+        toast.info(`Image ${imageToBeSaved.title.toUpperCase()} was saved`);
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
     }
   };
   return (
@@ -94,6 +106,7 @@ const App = () => {
               </Container>
             </>
         )}
+        <ToastContainer position="bottom-right"/>
       </div>
   );
 };
